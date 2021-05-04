95ec310248e76a8d552aaba2bf5191455c02c8ad
JawalArcilla <>
2021-05-04 16:50:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let answer = 0
+let baseA = 0
+let baseB = 0
+let height = 0
+
+document.getElementById('button').addEventListener('click', calculate)
+
+function calculate () {
+
+baseA = document.getElementById('base-a').value
+
+baseB = document.getElementById('base-b').value
+
+height = document.getElementById('height').value
+
+answer = ((baseA + baseB) / 2) * height
+
+document.getElementById('answer-id').innerHTML = answer
+}
