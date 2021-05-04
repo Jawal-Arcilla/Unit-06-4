8ba778a30a1ccf50514f7f2a18ff81a7c1ff2632
JawalArcilla <>
2021-05-04 17:00:19 +0000

    fixed changes

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ let height = 0
 
 document.getElementById('button').addEventListener('click', calculate)
 
-function calculate () {
+function calculate (){
 
-baseA = document.getElementById('base-a').value
+  baseA = document.getElementById('base-a').value
 
-baseB = document.getElementById('base-b').value
+  baseB = document.getElementById('base-b').value
 
-height = document.getElementById('height').value
+  height = document.getElementById('height').value
 
-answer = ((baseA + baseB) / 2) * height
+  answer = ((baseA + baseB) / 2) * height
 
-document.getElementById('answer-id').innerHTML = answer
+  document.getElementById('answer-id').innerHTML = answer
 }
